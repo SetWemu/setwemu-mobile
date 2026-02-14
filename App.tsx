c6b7a6e90090841f7ac922babf744eeb1934dcad
Isa Farman <isafarman10@gmail.com>
2026-02-14 18:42:37 +0530

    feat: configure stack navigation and route chat screen
    Configured createStackNavigator in AppNavigator and set ChatListScreen as the initial default route. Wrapped root app component in SafeAreaProvider to fix display bounds.

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './src/navigation/AppNavigator';
 
-function App(): React.JSX.Element {
-  return <AppNavigator />;
-}
+const App = () => {
+  return (
+    <SafeAreaProvider>
+      <AppNavigator />
+    </SafeAreaProvider>
+  );
+};
 
 export default App;
